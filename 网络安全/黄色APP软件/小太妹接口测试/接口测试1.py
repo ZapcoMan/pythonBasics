69c37af08df98d8e7fc409eeeda07509a1103f11
codervibe <3507557934@qq.com>
2024-12-24 16:05:18 +0800

    添加 代码注释

--- 网络安全/黄色APP软件/小太妹接口测试/接口测试1.py
+++ 网络安全/黄色APP软件/小太妹接口测试/接口测试1.py
@@ -19,7 +19,7 @@ page=5&id=271
 """
 import requests
 
-# 定义请求头部
+# 定义请求头部，包含访问API所需的各类信息和令牌
 headers = {
     'Referer': 'https://weibo.com',
     'Device-Type': 'A',
@@ -35,19 +35,23 @@ headers = {
     'User-Agent': 'okhttp/4.11.0'
 }
 
-# 定义目标URL
+# 定义目标URL，即要请求的API地址
 url = 'https://api-m3-b.shunchen-gongsi.com/api/v2/module/module_movie_change'
 
-# 构建请求参数
+# 构建请求参数，此处为要传递给API的具体数据
 data = {
     'page': 1,
     'id': 271
 }
 
-# 发送 POST 请求
+# 发送 POST 请求，并保存返回的响应
 response = requests.post(url, headers=headers, data=data)
 
-# 解析响应内容为 JSON 格式
+# 解析响应内容为 JSON 格式，以便于后续处理
 json_data = response.text
+
+# 设置响应的编码方式为utf-8，确保正确解析响应内容
 response.encoding = 'utf-8'
+
+# 打印解析后的响应内容
 print(response.text)
