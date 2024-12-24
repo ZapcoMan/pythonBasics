02097d0a07e70bf722e318358a6ba15cfd278a4e
codervibe <3507557934@qq.com>
2024-12-24 18:35:50 +0800

    添加 第一个python程序

---
+++ 基础/第一个python脚本.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/12/24 下午6:05
+# @Author  : codervibe
+# @File    : 第一个python脚本.py
+# @Project : pythonBasics
+if __name__ == '__main__':
+    print('Hello,world')
