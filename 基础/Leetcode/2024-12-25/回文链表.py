31f0db8acda9748c4374a1a8982e5761fd8aca08
codervibe <3507557934@qq.com>
2024-12-25 20:57:26 +0800

    添加题目介绍

--- 基础/Leetcode/2024-12-25/回文链表.py
+++ 基础/Leetcode/2024-12-25/回文链表.py
@@ -3,3 +3,13 @@
 # @Author  : codervibe
 # @File    : 回文链表.py
 # @Project : pythonBasics
+"""
+给你一个单链表的头节点 head ，请你判断该链表是否为
+回文链表
+。如果是，返回 true ；否则，返回 false
+"""
+from typing import Optional
+
+from functorch.dim import stack
+
+
