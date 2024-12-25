fbde97fe812c01cb492674f5778243e2073160d7
codervibe <3507557934@qq.com>
2024-12-26 07:54:22 +0800

    完成题目

--- 基础/Leetcode/2024-12-26/两两交换链表中的节点.py
+++ 基础/Leetcode/2024-12-26/两两交换链表中的节点.py
@@ -6,4 +6,27 @@
 """
 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
 """
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node0 = dummy = ListNode(next=head)  # 用哨兵节点简化代码逻辑
+        node1 = head
+        while node1 and node1.next:  # 至少有两个节点
+            node2 = node1.next
+            node3 = node2.next
+
+            node0.next = node2  # 0 -> 2
+            node2.next = node1  # 2 -> 1
+            node1.next = node3  # 1 -> 3
+
+            node0 = node1  # 下一轮交换，0 是 1
+            node1 = node3  # 下一轮交换，1 是 3
+        return dummy.next  # 返回新链表的头节点
+
 
