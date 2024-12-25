62b03c152dc8a036a36a897e2fb64ecb534c59f5
codervibe <3507557934@qq.com>
2024-12-25 19:56:18 +0800

    完成题目

--- 基础/Leetcode/矩阵置零.py
+++ 基础/Leetcode/矩阵置零.py
@@ -6,4 +6,26 @@
 """
 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
 """
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        不返回任何东西，而是就地修改矩阵。
+        """
+        zero = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    zero.append((i, j))
+
+        while len(zero) > 0:
+            x, y = zero.pop()
+            for i in range(len(matrix)):
+                for j in range(len(matrix[i])):
+                    if i == x or j == y:
+                        matrix[i][j] = 0
+
+        return
 
