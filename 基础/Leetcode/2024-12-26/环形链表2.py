c68b9b5f980d50ea0882e83e1232a9983cbdab2e
codervibe <3507557934@qq.com>
2024-12-26 06:10:10 +0800

    完成题目

--- 基础/Leetcode/2024-12-26/环形链表2.py
+++ 基础/Leetcode/2024-12-26/环形链表2.py
@@ -10,3 +10,23 @@
 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况
 不允许修改 链表。
 """
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        while True:
+            if not (fast and fast.next): return
+            fast, slow = fast.next.next, slow.next
+            if fast == slow: break
+        fast = head
+        while fast != slow:
+            fast, slow = fast.next, slow.next
+        return fast
