eddd9f0c87eb976b6c72d3ed97a1c5ca4c56828b
codervibe <3507557934@qq.com>
2024-12-26 07:37:48 +0800

    添加题目介绍

--- 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
+++ 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Time    : 26 12月 2024 7:36 上午
+# @Time    : 26 12月 2024 7:36 上午
 # @Author  : codervibe
 # @File    : 删除链表中倒数N个的节点.py
 # @Project : pythonBasics
+"""
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+"""
