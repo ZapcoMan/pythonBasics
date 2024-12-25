0bc5048f130f7e42e5bdc07f838e9a833ffcb2a7
codervibe <3507557934@qq.com>
2024-12-25 20:10:50 +0800

    修改代码 缩短内存使用的大小

--- 基础/Leetcode/螺旋矩阵.py
+++ 基础/Leetcode/螺旋矩阵.py
@@ -6,3 +6,45 @@
 """
 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
 """
+# 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        m, n = len(matrix), len(matrix[0])
+        res = []
+        left, right, top, bottom = 0, n - 1, 0, m - 1
+
+        while left <= right and top <= bottom:
+            # Traverse from left to right along the top row
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+
+            # Traverse from top to bottom along the right column
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                # Traverse from right to left along the bottom row
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                # Traverse from bottom to top along the left column
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+
+        return res
+
+
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(Solution().spiralOrder(matrix))
