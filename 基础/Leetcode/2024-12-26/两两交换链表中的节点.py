be96aa8769aad7ec5c5c12ab09bb40702313bb29
codervibe <3507557934@qq.com>
2024-12-26 07:54:59 +0800

    添加注释

--- 基础/Leetcode/2024-12-26/两两交换链表中的节点.py
+++ 基础/Leetcode/2024-12-26/两两交换链表中的节点.py
@@ -10,23 +10,42 @@ from typing import Optional
 
 
 class ListNode:
+    """链表节点类"""
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
+    """解决方案类"""
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node0 = dummy = ListNode(next=head)  # 用哨兵节点简化代码逻辑
+        """
+        两两交换链表中的节点。
+
+        不修改节点值，只通过改变节点间的指针来完成交换。
+
+        参数:
+        head: Optional[ListNode] - 输入链表的头节点。
+
+        返回:
+        Optional[ListNode] - 交换后链表的头节点。
+        """
+        # 使用哨兵节点简化边界处理
+        node0 = dummy = ListNode(next=head)
         node1 = head
-        while node1 and node1.next:  # 至少有两个节点
+        # 遍历链表，两两交换节点
+        while node1 and node1.next:
+            # 定义当前节点的下一个节点
             node2 = node1.next
+            # 保存下一个节点的下一个节点
             node3 = node2.next
 
+            # 交换节点
             node0.next = node2  # 0 -> 2
             node2.next = node1  # 2 -> 1
             node1.next = node3  # 1 -> 3
 
+            # 更新指针，为下一次交换做准备
             node0 = node1  # 下一轮交换，0 是 1
             node1 = node3  # 下一轮交换，1 是 3
-        return dummy.next  # 返回新链表的头节点
-
-
+        # 返回新链表的头节点
+        return dummy.next
