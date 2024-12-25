96b64e9c43d13cc50d496dadb491b2baa660cae9
codervibe <3507557934@qq.com>
2024-12-25 20:19:20 +0800

    完成提米

--- 基础/Leetcode/旋转图像.py
+++ 基础/Leetcode/旋转图像.py
@@ -9,3 +9,19 @@
 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
 """
 
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        不返回任何东西，而是就地修改矩阵。
+        """
+        size = len(matrix)
+        for r in range(size):
+            for c in range(r, size):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        for r in range(size):
+            left, right = 0, size - 1
+            while left < right:
+                matrix[r][left], matrix[r][right] = matrix[r][right], matrix[r][left]
+                left += 1
+                right -= 1
+        return
