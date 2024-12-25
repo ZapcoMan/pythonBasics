cdf94fff92aca5abb95ec6c654f99d79cad8aa90
codervibe <3507557934@qq.com>
2024-12-25 20:57:44 +0800

    完成题目

--- 基础/Leetcode/2024-12-25/回文链表.py
+++ 基础/Leetcode/2024-12-25/回文链表.py
@@ -13,3 +13,37 @@ from typing import Optional
 from functorch.dim import stack
 
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # 给你一个单链表的头节点 head ，请你判断该链表是否为
+        # 回文链表
+        if not head:
+            return True
+        while head:
+            if head.val != stack.pop():
+                return False
+            head = head.next
+            return True
+
+        def reverseList(head: ListNode) -> ListNode:
+            pre = None
+            cur = head
+            while cur:
+                tmp = cur.next
+                cur.next = pre
+                pre = cur
+                cur = tmp
+            return pre
+
+        while head:
+            if head.val != stack.pop():
+                return False
+            head = head.next
+            return True
