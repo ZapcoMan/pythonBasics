fdae125cb356507352113055d73427cd51e5c1e3
codervibe <3507557934@qq.com>
2024-12-25 19:23:44 +0800

    添加 合并区间脚本

---
+++ 基础/Leetcode/合并区间.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 25 12月 2024 7:21 下午
+# @Author  : codervibe
+# @File    : 合并区间.py
+# @Project : pythonBasics
+"""
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+"""
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda p: p[0])  # 按照左端点从小到大排序
+        ans = []
+        for p in intervals:
+            if ans and p[0] <= ans[-1][1]:  # 可以合并
+                ans[-1][1] = max(ans[-1][1], p[1])  # 更新右端点最大值
+            else:  # 不相交，无法合并
+                ans.append(p)  # 新的合并区间
+        return ans
+
