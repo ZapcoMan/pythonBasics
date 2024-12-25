e1b44140d4b93268bb5450a8d2c42244d146378f
codervibe <3507557934@qq.com>
2024-12-26 07:35:43 +0800

    添加注释 因为 性能很好 所以不需要优化 时间复杂度和空间复杂度

--- 基础/Leetcode/2024-12-26/两数相加.py
+++ 基础/Leetcode/2024-12-26/两数相加.py
@@ -20,11 +20,31 @@ class ListNode:
 class Solution:
     # l1 和 l2 为当前遍历的节点，carry 为进位
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
-        if l1 is None and l2 is None:  # 递归边界：l1 和 l2 都是空节点
-            return ListNode(carry) if carry else None  # 如果进位了，就额外创建一个节点
-        if l1 is None:  # 如果 l1 是空的，那么此时 l2 一定不是空节点
-            l1, l2 = l2, l1  # 交换 l1 与 l2，保证 l1 非空，从而简化代码
-        s = carry + l1.val + (l2.val if l2 else 0)  # 节点值和进位加在一起
-        l1.val = s % 10  # 每个节点保存一个数位（直接修改原链表）
-        l1.next = self.addTwoNumbers(l1.next, l2.next if l2 else None, s // 10)  # 进位
+        """
+        递归地将两个链表 l1 和 l2 对应位置的节点值相加，并处理进位。
+
+        参数:
+        l1: 第一个链表的当前节点
+        l2: 第二个链表的当前节点
+        carry: 从低位进位的值，默认为0
+
+        返回:
+        返回相加后的链表的头节点
+        """
+        # 递归边界：l1 和 l2 都是空节点
+        if l1 is None and l2 is None:
+            # 如果进位了，就额外创建一个节点，否则返回None
+            return ListNode(carry) if carry else None
+        # 如果 l1 是空的，那么此时 l2 一定不是空节点
+        if l1 is None:
+            # 交换 l1 与 l2，保证 l1 非空，从而简化代码
+            l1, l2 = l2, l1
+        # 节点值和进位加在一起
+        s = carry + l1.val + (l2.val if l2 else 0)
+        # 每个节点保存一个数位（直接修改原链表）
+        l1.val = s % 10
+        # 递归处理下一对节点，并传递进位
+        l1.next = self.addTwoNumbers(l1.next, l2.next if l2 else None, s // 10)
+        # 返回相加后的链表的头节点
         return l1
+
