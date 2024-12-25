fc4b30355bc3ba46d40ff4e86745e5e2cc4a5c75
codervibe <3507557934@qq.com>
2024-12-25 20:46:39 +0800

    生成注释

--- 基础/Leetcode/2024-12-25/搜索二维矩阵 II.py
+++ 基础/Leetcode/2024-12-25/搜索二维矩阵 II.py
@@ -14,6 +14,19 @@ from typing import List
 # 官方题解
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        在二维矩阵中搜索目标值。
+
+        第一个方法是简单的暴力搜索方法，遍历矩阵中的每个元素，直到找到目标值或遍历完所有元素。
+        这种方法没有利用矩阵行和列的排序特性。
+
+        参数:
+        matrix (List[List[int]]): 二维矩阵，每一行的元素从左到右按升序排列，每一列的元素从上到下按升序排列。
+        target (int): 要搜索的目标值。
+
+        返回:
+        bool: 如果找到目标值则返回True，否则返回False。
+        """
         for row in matrix:
             for element in row:
                 if element == target:
@@ -21,6 +34,20 @@ class Solution:
         return False
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        在二维矩阵中搜索目标值。
+
+        第二个方法是利用矩阵行和列的排序特性的搜索方法，从矩阵的右上角开始搜索。
+        如果当前元素等于目标值，则返回True；如果当前元素小于目标值，则排除这一行；如果当前元素大于目标值，则排除这一列。
+        这种方法有效地减少了需要搜索的元素数量。
+
+        参数:
+        matrix (List[List[int]]): 二维矩阵，每一行的元素从左到右按升序排列，每一列的元素从上到下按升序排列。
+        target (int): 要搜索的目标值。
+
+        返回:
+        bool: 如果找到目标值则返回True，否则返回False。
+        """
         m, n = len(matrix), len(matrix[0])
         i, j = 0, n - 1  # 从右上角开始
         while i < m and j >= 0:  # 还有剩余元素
