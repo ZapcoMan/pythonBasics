efc51fcb2f6479b1df78be905a64c23b767f46e0
codervibe <3507557934@qq.com>
2024-12-25 19:25:09 +0800

    添加注释

--- 基础/Leetcode/合并区间.py
+++ 基础/Leetcode/合并区间.py
@@ -10,22 +10,48 @@
 """
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda p: p[0])  # 按照左端点从小到大排序
+        """
+        合并所有重叠的区间，并返回一个不重叠的区间数组。
+
+        参数:
+        intervals: List[List[int]] - 一个二维列表，其中每个子列表代表一个区间的左右端点。
+
+        返回值:
+        List[List[int]] - 合并重叠区间后，返回的不重叠区间列表。
+        """
+        # 按照区间的左端点进行排序，以便后续合并处理
+        intervals.sort(key=lambda p: p[0])
         ans = []
         for p in intervals:
-            if ans and p[0] <= ans[-1][1]:  # 可以合并
-                ans[-1][1] = max(ans[-1][1], p[1])  # 更新右端点最大值
-            else:  # 不相交，无法合并
-                ans.append(p)  # 新的合并区间
+            # 判断当前区间是否与上一个区间重叠
+            if ans and p[0] <= ans[-1][1]:
+                # 如果重叠，更新最后一个区间的右端点为两个区间右端点的最大值
+                ans[-1][1] = max(ans[-1][1], p[1])
+            else:
+                # 如果不重叠，将当前区间作为新的合并区间加入结果列表
+                ans.append(p)
         return ans
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x: x[0])
+        """
+        合并所有重叠的区间，并返回一个不重叠的区间数组。
+
+        参数:
+        intervals: List[List[int]] - 一个二维列表，其中每个子列表代表一个区间的左右端点。
+
+        返回值:
+        List[List[int]] - 合并重叠区间后，返回的不重叠区间列表。
+        """
+        # 按照区间的左端点进行排序，以便后续合并处理
+        intervals.sort(key=lambda x: x[0])
         merge = []
 
         for interval in intervals:
+            # 判断当前区间是否与上一个区间重叠
             if not merge or merge[-1][1] < interval[0]:
+                # 如果不重叠，将当前区间加入合并列表
                 merge.append(interval)
             else:
+                # 如果重叠，更新最后一个区间的右端点为两个区间右端点的最大值
                 merge[-1][1] = max(merge[-1][1], interval[1])
         return merge
