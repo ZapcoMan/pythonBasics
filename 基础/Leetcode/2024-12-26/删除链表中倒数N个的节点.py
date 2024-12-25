702ca8a1620ca7b39cbaaaf8d6170c641f44bbf6
codervibe <3507557934@qq.com>
2024-12-26 07:42:31 +0800

    添加注释

--- 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
+++ 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
@@ -8,20 +8,23 @@
 """
 from typing import Optional
 
-
+# 定义链表节点类
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-
+# 定义解决方案类
 class Solution:
+    # 从链表的倒数第n个节点开始删除
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # 初始化两个指针，left和right，dummy是一个虚拟节点，简化边界情况处理
         left = right = dummy = ListNode(next=head)
+        # right指针向前移动n步，目的是使left和right之间保持n的距离
         for _ in range(n):
             right = right.next
+        # 移动left和right，直到right到达链表末尾，此时left指向倒数第n个节点的前一个节点
         while right.next:
             left = left.next
             right = right.next
-        left.next = left.next.next
-        return dummy.next
+        # 跳过le
