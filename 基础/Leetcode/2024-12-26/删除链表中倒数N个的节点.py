eac22eb0f2588aa5cc1bf37b63eb94783f15b8af
codervibe <3507557934@qq.com>
2024-12-26 07:41:14 +0800

    完成题目

--- 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
+++ 基础/Leetcode/2024-12-26/删除链表中倒数N个的节点.py
@@ -6,3 +6,22 @@
 """
 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 """
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        left = right = dummy = ListNode(next=head)
+        for _ in range(n):
+            right = right.next
+        while right.next:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummy.next
