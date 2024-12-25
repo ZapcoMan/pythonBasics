91290ddf30c21bc8f9904eb8bcb78f430493e51e
codervibe <3507557934@qq.com>
2024-12-26 06:00:42 +0800

    添加性能更加好的 方法

--- 基础/Leetcode/2024-12-26/环形链表.py
+++ 基础/Leetcode/2024-12-26/环形链表.py
@@ -31,4 +31,11 @@ class Solution:
         return False
 
 
-
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
