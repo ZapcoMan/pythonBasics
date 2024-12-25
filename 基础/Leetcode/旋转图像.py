2ade6962554debb1f33a204a3abf695923fc5028
codervibe <3507557934@qq.com>
2024-12-25 20:24:13 +0800

    添加最优解函数

--- 基础/Leetcode/旋转图像.py
+++ 基础/Leetcode/旋转图像.py
@@ -8,6 +8,7 @@
 
 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
 """
+from typing import List
 
 
 #
@@ -40,6 +41,31 @@ class Solution:
 
         # 旋转完成后，不返回任何值
         return
+
+    # 添加最优解
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        不返回任何东西，而是就地修改矩阵。
+        """
+
+        def next_position(i, j, di, dj, steps):
+            while steps:
+                if not (left <= i + di <= right and top <= j + dj <= bottom):
+                    di, dj = dj, -di
+                steps, i, j = steps - 1, i + di, j + dj
+            return i, j, di, dj
+
+        def move_4_points(i, j, steps):
+            to_be_moved, di, dj = matrix[i][j], 0, 1
+            for _ in range(4):
+                next_i, next_j, di, dj = next_position(i, j, di, dj, steps)
+                matrix[next_i][next_j], to_be_moved, i, j = to_be_moved, matrix[next_i][next_j], next_i, next_j
+
+        left, right, top, bottom = 0, len(matrix) - 1, 0, len(matrix) - 1
+        while left < right:
+            for j in range(left, right):
+                move_4_points(top, j, right - left)
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
 
 
 if __name__ == '__main__':
