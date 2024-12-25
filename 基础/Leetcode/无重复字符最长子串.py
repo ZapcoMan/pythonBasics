c79b77158ddd09609eb8ef48030b73049f8f47db
codervibe <3507557934@qq.com>
2024-12-25 09:47:19 +0800

    添加注释

--- 基础/Leetcode/无重复字符最长子串.py
+++ 基础/Leetcode/无重复字符最长子串.py
@@ -19,8 +19,39 @@ class Solution:
             res = max(res, j - i) # 更新结果
         return res
 
-"""
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # 寻找最长无重复字符的子串长度。
+        # 
+        # 通过维护一个滑动窗口，使用字典记录每个字符最后出现的位置，来实现。
+        # 
+        # 参数:
+        # s: 输入的字符串。
+        # 
+        # 返回:
+        # 最长无重复字符子串的长度。
+        n = len(s)
+        # 如果字符串长度小于2，直接返回其长度，因为不存在重复字符的子串
+        if n < 2: return n
+        # 使用defaultdict来存储字符的索引，方便快速查找字符是否已经出现过
+        dic = collections.defaultdict() # 存储key元素的最右边idex
+        begin, max_len = 0, 1
+        # 遍历字符串，使用enumerate同时获取索引和字符
+        for i, c in enumerate(s):
+            # 如果字符已经在字典中，并且上次出现的位置在当前窗口内
+            if c in dic and dic[c] >= begin: 
+                # 更新最大长度为当前索引到窗口开始位置的距离
+                max_len = max(max_len, i - begin)
+                # 移动窗口的开始位置到重复字符的下一个位置
+                begin = dic[c] + 1
+            # 更新或添加当前字符的索引到字典中
+            dic[c] = i
+        # 最后，确保检查从最后一个重复字符到字符串末尾的距离是否为最大长度
+        max_len = max(max_len, n - begin) # 统计最后不重复子串长度
+        return max_len
+
+"""
 
 class Solution:
     @staticmethod
@@ -54,18 +85,4 @@ class Solution:
         return max_len
 
 
-"""
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        if n < 2: return n
-        dic = collections.defaultdict() # 存储key元素的最右边idex
-        begin, max_len = 0, 1
-        for i, c in enumerate(s):
-            if c in dic and dic[c] >= begin: # 当前跟之前重复，先计算之前的最大长度
-                max_len = max(max_len, i - begin)
-                begin = dic[c] + 1
-            dic[c] = i
-        max_len = max(max_len, n - begin) # 统计最后不重复子串长度
-        return max_len
-"""
+
