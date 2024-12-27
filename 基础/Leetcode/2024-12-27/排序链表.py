2decf61879c867c16d8ee01d246503b06f9778c4
codervibe <3507557934@qq.com>
2024-12-27 15:10:56 +0800

    完成题目介绍

--- 基础/Leetcode/2024-12-27/排序链表.py
+++ 基础/Leetcode/2024-12-27/排序链表.py
@@ -6,3 +6,31 @@
 """
 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
 """
+
+
+# 单链表的定义。
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next: return head # termination.
+        # cut the LinkedList at the mid index.
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None # save and cut.
+        # recursive for cutting.
+        left, right = self.sortList(head), self.sortList(mid)
+        # merge `left` and `right` linked list and return it.
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val: h.next, left = left, left.next
+            else: h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+
+
+
