6785569d0d76f9b11f209cb114ab3e8f2664975d
codervibe <3507557934@qq.com>
2024-12-27 15:13:45 +0800

    添加注释

--- 基础/Leetcode/2024-12-27/排序链表.py
+++ 基础/Leetcode/2024-12-27/排序链表.py
@@ -4,33 +4,57 @@
 # @File    : 排序链表.py
 # @Project : pythonBasics
 """
-给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表。
 """
 
 
 # 单链表的定义。
 class ListNode:
     def __init__(self, val=0, next=None):
+        """
+        初始化单链表节点。
+
+        参数:
+        val (int): 节点的值，默认为0。
+        next (ListNode): 下一个节点，默认为None。
+        """
         self.val = val
         self.next = next
+
+
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        if not head or not head.next: return head # termination.
-        # cut the LinkedList at the mid index.
+        """
+        使用归并排序对链表进行升序排序。
+
+        参数:
+        head (ListNode): 链表的头节点。
+
+        返回:
+        ListNode: 排序后链表的头节点。
+        """
+        # 如果链表为空或只有一个节点，直接返回链表（已排序）
+        if not head or not head.next:
+            return head
+
+        # 使用快慢指针找到链表的中间节点，并将链表从中点分割成两部分
         slow, fast = head, head.next
         while fast and fast.next:
             fast, slow = fast.next.next, slow.next
-        mid, slow.next = slow.next, None # save and cut.
-        # recursive for cutting.
+        mid, slow.next = slow.next, None  # 保存中点并切断链表
+
+        # 递归地对左右两部分链表进行排序
         left, right = self.sortList(head), self.sortList(mid)
-        # merge `left` and `right` linked list and return it.
+
+        # 合并两个有序链表
         h = res = ListNode(0)
         while left and right:
-            if left.val < right.val: h.next, left = left, left.next
-            else: h.next, right = right, right.next
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
             h = h.next
         h.next = left if left else right
+
         return res.next
-
-
 
