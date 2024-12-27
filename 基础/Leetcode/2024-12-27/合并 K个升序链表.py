305d70bccff92f96ba34af3097cae08b6f033ad5
codervibe <3507557934@qq.com>
2024-12-27 15:31:42 +0800

    完成题目

--- 基础/Leetcode/2024-12-27/合并 K个升序链表.py
+++ 基础/Leetcode/2024-12-27/合并 K个升序链表.py
@@ -8,3 +8,25 @@
 
 请你将所有链表合并到一个升序链表中，返回合并后的链表。
 """
+from heapq import heapify, heappop, heappush
+from typing import Optional, List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        cur = dummy = ListNode()  # 哨兵节点，作为合并后链表头节点的前一个节点
+        h = [head for head in lists if head]  # 初始把所有链表的头节点入堆
+        heapify(h)  # 堆化
+        while h:  # 循环直到堆为空
+            node = heappop(h)  # 剩余节点中的最小节点
+            if node.next:  # 下一个节点不为空
+                heappush(h, node.next)  # 下一个节点有可能是最小节点，入堆
+            cur.next = node  # 合并到新链表中
+            cur = cur.next  # 准备合并下一个节点
+        return dummy.next  # 哨兵节点的下一个节点就是新链表的头节点
+
+
