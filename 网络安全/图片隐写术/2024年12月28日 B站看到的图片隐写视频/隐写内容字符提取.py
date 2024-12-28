898509d2dea9c18ce6eca1541376d67e01ae63a2
codervibe <3507557934@qq.com>
2024-12-28 11:19:42 +0800

    完成　并找到最终的图片

--- 网络安全/图片隐写术/2024年12月28日 B站看到的图片隐写视频/隐写内容字符提取.py
+++ 网络安全/图片隐写术/2024年12月28日 B站看到的图片隐写视频/隐写内容字符提取.py
@@ -6,3 +6,13 @@
 """
 使用kali 的 steghide  工具提取  提取命令 ：steghide extract -sf hack.jpg
 """
+ext_Infor = "Perhaps you see this and wonder what the fuck is going on, What is this bullshit. Well, the solution is Right in front of your eyes, you just have to look closer. Just follow the numbers and you will find the answer. Did you find it?"
+index = ["0", "4", "6", "16", "29", "72", "78", "99", "161", "155", "157", "181", "/", "163", "144", "104", "217", "3",
+         "227", "182", "104"]
+
+for msg in index:
+    if msg == '/':
+        print("/", end='')
+    else:
+        print(ext_Infor[int(msg)], end='')
+print()
