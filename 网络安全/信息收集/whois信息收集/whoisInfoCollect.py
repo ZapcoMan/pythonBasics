c3d0e50a74d7b16bc0c4c3e83b61a63fbded4474
codervibe <3507557934@qq.com>
2024-12-30 18:23:36 +0800

    完成查询的基本功能

--- 网络安全/信息收集/whois信息收集/whoisInfoCollect.py
+++ 网络安全/信息收集/whois信息收集/whoisInfoCollect.py
@@ -3,3 +3,35 @@
 # @Author  : codervibe
 # @File    : whoisInfoCollect.py
 # @Project : pythonBasics
+import whois
+
+
+def is_registered(domain):
+    """
+    检查域名是否已被注册
+    """
+    try:
+        whois_info = whois.whois(domain)
+        # print(whois_info)
+
+        # return whois_info.domain_name is not None
+    except Exception:
+        return False
+    else:
+        return bool(whois_info.domain_name)
+
+if __name__ == '__main__':
+    domainList = {
+        "baidu.com",
+        "google.com",
+        "amazon.com",
+        "facebook.com",
+        "twitter.com"
+    }
+    for domain in domainList:
+        # print(f'{domain}')
+        if is_registered(domain):
+            print(f'{domain}: is registered')
+        else:
+            print(f'{domain} is not registered')
+    # print(is_registered("baidu.com"))
