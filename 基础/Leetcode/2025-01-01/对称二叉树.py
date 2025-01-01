e60a3574d5cd5ff983b5eb38dd50654253a05bed
codervibe <3507557934@qq.com>
2025-01-01 11:47:15 +0800

    添加注释

--- 基础/Leetcode/2025-01-01/对称二叉树.py
+++ 基础/Leetcode/2025-01-01/对称二叉树.py
@@ -20,13 +20,30 @@ class TreeNode:
 class Solution:
     # 如果是中序遍历 那么中序遍历后的逆序和自己本身是一样的
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        """
+        判断给定的二叉树是否为对称二叉树。
+
+        参数:
+        root: Optional[TreeNode] - 二叉树的根节点。
+
+        返回值:
+        bool - 如果二叉树是对称的，则返回True；否则返回False。
+        """
+        # 初始化当前节点、结果列表和栈
         cur, res, stack = root, [], []
+        # 遍历二叉树
         while stack or cur:
+            # 尽可能地遍历左子树
             while cur:
                 stack.append(cur)
                 cur = cur.left
+            # 回溯到上一个节点
             cur = stack.pop()
+            # 将节点值添加到结果列表中
             res.append(cur.val)
+            # 遍历右子树
             cur = cur.right
+        # 生成结果列表的逆序列表
         ress = res[::-1]
+        # 判断原列表和逆序列表是否相同，从而确定二叉树是否对称
         return res == ress
