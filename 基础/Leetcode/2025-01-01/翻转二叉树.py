203c7933ba60a115e18945f378a14fb1db981944
codervibe <3507557934@qq.com>
2025-01-01 11:03:29 +0800

    添加注释

--- 基础/Leetcode/2025-01-01/翻转二叉树.py
+++ 基础/Leetcode/2025-01-01/翻转二叉树.py
@@ -1,23 +1,40 @@
 # -*- coding: utf-8 -*-
-# @Time    : 01 1月 2025 10:49 上午
+# @Time    : 01 1月 2025 10:24 上午
 # @Author  : codervibe
-# @File    : 翻转二叉树.py
+# @File    : 二叉树的最大深度.py
 # @Project : pythonBasics
 """
-给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
 """
 from typing import Optional
 
 
+# Definition for a binary tree node.
 class TreeNode:
+    """定义二叉树节点类"""
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # print(f"{root}")
-        if root:
-            root.left,root.right = self.invertTree(root.right),self.invertTree(root.left)
-            return root
-        return root
+    """提供解决方案的类"""
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        """
+        计算二叉树的最大深度
+
+        采用递归方法，如果当前节点不为空，则计算其左右子树的最大深度，并返回较大值加1；
+        如果当前节点为空，说明到达叶子节点，返回0
+
+        :param root: 二叉树的根节点
+        :return: 最大深度
+        """
+        print(f"{root}")
+        if root is not None:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        return 0
+
