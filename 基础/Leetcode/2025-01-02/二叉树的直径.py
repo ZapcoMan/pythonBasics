b096c71b54a73a15e910673d68a3e33d49365de6
codervibe <3507557934@qq.com>
2025-01-02 19:57:01 +0800

    完成代码

--- 基础/Leetcode/2025-01-02/二叉树的直径.py
+++ 基础/Leetcode/2025-01-02/二叉树的直径.py
@@ -10,3 +10,26 @@
 
 两节点之间路径的 长度 由它们之间边数表示。
 """
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 1
+
+        def depth(root):
+            if not root: return 0
+            L = depth(root.left)
+            R = depth(root.right)
+            self.ans = max(self.ans, L + R + 1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
