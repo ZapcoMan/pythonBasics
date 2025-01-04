be3ca16ecee4ddac1a8424368b075f04095ff40b
codervibe <3507557934@qq.com>
2025-01-04 15:26:57 +0800

    完成题目

--- 基础/Leetcode/2024-01-04/二叉树搜索第K小的元素.py
+++ 基础/Leetcode/2024-01-04/二叉树搜索第K小的元素.py
@@ -3,3 +3,31 @@
 # @Author  : codervibe
 # @File    : 二叉树搜索第K小的元素.py
 # @Project : pythonBasics
+"""
+给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 小的元素（从 1 开始计数）。
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            # if self.k == 0: return
+            self.k -= 1
+            if self.k == 0:
+                self.res = root.val
+            dfs(root.right)
+
+        self.k = k
+        dfs(root)
+        return self.res
