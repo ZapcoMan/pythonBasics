7bc70fff1b04cc856f6eafc6e885814c522dd1a8
codervibe <3507557934@qq.com>
2025-01-04 15:49:23 +0800

    完成题目

--- 基础/Leetcode/2025-01-04/二叉树的右视图.py
+++ 基础/Leetcode/2025-01-04/二叉树的右视图.py
@@ -6,3 +6,31 @@
 """
 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
 """
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        rightmost_value_at_depth = dict() # 深度为索引，存放节点的值
+        max_depth = -1
+
+        stack = [(root, 0)]
+        while stack:
+            node, depth = stack.pop()
+
+            if node is not None:
+                # 维护二叉树的最大深度
+                max_depth = max(max_depth, depth)
+
+                # 如果不存在对应深度的节点我们才插入
+                rightmost_value_at_depth.setdefault(depth, node.val)
+
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth + 1)]
