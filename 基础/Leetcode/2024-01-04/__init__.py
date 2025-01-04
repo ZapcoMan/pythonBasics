70a545adeb94accb07534e4f916e7333a026a887
codervibe <3507557934@qq.com>
2025-01-04 14:43:49 +0800

    添加今天的第一道题目

---
+++ 基础/Leetcode/2024-01-04/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 04 1月 2025 2:40 下午
+# @Author  : codervibe
+# @File    : __init__.py.py
+# @Project : pythonBasics
---
+++ 基础/Leetcode/2024-01-04/二叉树搜索第K小的元素.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 04 1月 2025 2:43 下午
+# @Author  : codervibe
+# @File    : 二叉树搜索第K小的元素.py
+# @Project : pythonBasics
