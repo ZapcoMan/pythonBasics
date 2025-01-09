4f6b2f5a435e632820cd7e410123ba9973e0a50a
codervibe <3507557934@qq.com>
2025-01-09 18:57:32 +0800

    添加 题目的详情

--- 基础/Leetcode/2025-01-09/从前序与中序遍历序列构造二叉树.py
+++ 基础/Leetcode/2025-01-09/从前序与中序遍历序列构造二叉树.py
@@ -7,3 +7,29 @@
 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
 """
 
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        dic, preorder = {}, preorder
+
+        def recur(root, left, right):
+            if left > right:
+                return
+            node = TreeNode(preorder[root])
+            i = dic[preorder[root]]
+            node.left = recur(root + 1, left, i - 1)
+            node.right = recur(i - left + root + 1, i + 1, right)
+            return node
+
+        for i in range(len(inorder)):
+            dic[inorder[i]] = i
+        return recur(0, 0, len(inorder) - 1)
