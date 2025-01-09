1eb83c94fbe46987dc1dcbf0240a02a47bed1bc5
codervibe <3507557934@qq.com>
2025-01-09 19:40:13 +0800

    完成题目

--- 基础/Leetcode/2025-01-09/二叉树中的最大路径和.py
+++ 基础/Leetcode/2025-01-09/二叉树中的最大路径和.py
@@ -3,3 +3,35 @@
 # @Author  : codervibe
 # @File    : 二叉树中的最大路径和.py
 # @Project : pythonBasics
+"""
+二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。
+同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+
+路径和 是路径中各节点值的总和。
+
+给你一个二叉树的根节点 root ，返回其 最大路径和 。
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.maxsum = float('-inf')
+
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.maxsum = max(self.maxsum, root.val + left + right)
+            return max(0, max(left, right) + root.val)
+
+        print(dfs(root))
+        return self.maxsum
