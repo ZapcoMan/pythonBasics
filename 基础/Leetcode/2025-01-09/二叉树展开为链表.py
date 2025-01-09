4251d6dc86f6ddcbae4b0fab2ed29f31632a796d
codervibe <3507557934@qq.com>
2025-01-09 19:06:46 +0800

    第一道题 添加注释

--- 基础/Leetcode/2025-01-09/二叉树展开为链表.py
+++ 基础/Leetcode/2025-01-09/二叉树展开为链表.py
@@ -11,24 +11,28 @@
 """
 from typing import Optional
 
-
 class TreeNode:
+    """定义二叉树节点类"""
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-
 class Solution:
-    head = None
+    head = None  # 初始化链表头节点为空
+
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
-        不要返回任何东西，而是就地修改根。
+        将二叉树就地展开为链表，不需要返回任何东西，而是就地修改根。
+        展开后的链表顺序与二叉树的先序遍历顺序相同。
+
+        参数:
+        root (Optional[TreeNode]): 二叉树的根节点
         """
         if root is None:
-            return
-        self.flatten(root.right)
-        self.flatten(root.left)
-        root.left = None
-        root.right = self.head
-        self.head = root
+            return  # 如果节点为空，则直接返回
+        self.flatten(root.right)  # 递归处理右子树
+        self.flatten(root.left)  # 递归处理左子树
+        root.left = None  # 将当前节点的左子节点设为空
+        root.right = self.head  # 将当前节点的右子节点设为链表的头节点
+        self.head = root  # 更新链表的头节点为当前节点
