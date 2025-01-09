aea735eb753756ae62be4275ee97caa1cfd49bad
codervibe <3507557934@qq.com>
2025-01-09 18:45:05 +0800

    完成题目

--- 基础/Leetcode/2025-01-09/二叉树展开为链表.py
+++ 基础/Leetcode/2025-01-09/二叉树展开为链表.py
@@ -9,3 +9,26 @@
 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
 展开后的单链表应该与二叉树 先序遍历 顺序相同。
 """
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    head = None
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        不要返回任何东西，而是就地修改根。
+        """
+        if root is None:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.left = None
+        root.right = self.head
+        self.head = root
