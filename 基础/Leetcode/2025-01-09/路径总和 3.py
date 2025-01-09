47b6577cbf876f749cac551a6ea2dca8068371e0
codervibe <3507557934@qq.com>
2025-01-09 19:05:13 +0800

    完成题目

--- 基础/Leetcode/2025-01-09/路径总和 3.py
+++ 基础/Leetcode/2025-01-09/路径总和 3.py
@@ -8,3 +8,33 @@
 
 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
 """
+from collections import defaultdict
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        ans = 0
+        cnt = defaultdict(int)
+        cnt[0] = 1
+
+        def dfs(node: Optional[TreeNode], s: int) -> None:
+            if node is None:
+                return
+            nonlocal ans
+            s += node.val
+            ans += cnt[s - targetSum]
+            cnt[s] += 1
+            dfs(node.left, s)
+            dfs(node.right, s)
+            cnt[s] -= 1
+
+        dfs(root, 0)
+        return ans
