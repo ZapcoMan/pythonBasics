d8cba8fd546d848bc0e8fef5f2cefb58a0b8f672
ZapcoMan <3507557934@qq.com>
2025-02-09 18:58:46 +0800

    完成题目

--- 基础/Leetcode/2025-02-09/课程表.py
+++ 基础/Leetcode/2025-02-09/课程表.py
@@ -11,3 +11,20 @@
 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
 """
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = defaultdict(list)
+        indeg = [0] * numCourses
+        for a, b in prerequisites:
+            g[b].append(a)
+            indeg[a] += 1
+        cnt = 0
+        q = deque(i for i, x in enumerate(indeg) if x == 0)
+        while q:
+            i = q.popleft()
+            cnt += 1
+            for j in g[i]:
+                indeg[j] -= 1
+                if indeg[j] == 0:
+                    q.append(j)
+        return cnt == numCourses
