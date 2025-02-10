3dbe941862d2011db6ae352f47e3a5fcf0a5de57
ZapcoMan <3507557934@qq.com>
2025-02-10 23:16:22 +0800

    添加注释

--- 基础/Leetcode/2025-02-10/栈/字符串解码.py
+++ 基础/Leetcode/2025-02-10/栈/字符串解码.py
@@ -15,21 +15,42 @@
 
 """
 class Solution:
+    """
+    解码字符串类
+    """
     def decodeString(self, s: str) -> str:
+        """
+        解码字符串主函数
+
+        递归地解析字符串，将数字代表的重复次数与中括号内的字符串相乘，并累加到结果字符串中。
+
+        :param s: 编码后的字符串
+        :return: 解码后的字符串
+        """
         def dfs(s, i):
+            """
+            深度优先搜索函数，用于解码字符串
+
+            :param s: 待解码的字符串
+            :param i: 当前处理到的字符串索引
+            :return: 解码后的字符串和下一个要处理的索引位置
+            """
             res, multi = "", 0
             while i < len(s):
                 if '0' <= s[i] <= '9':
+                    # 处理数字，计算重复次数
                     multi = multi * 10 + int(s[i])
                 elif s[i] == '[':
+                    # 遇到左括号，递归处理括号内的字符串
                     i, tmp = dfs(s, i + 1)
                     res += multi * tmp
                     multi = 0
                 elif s[i] == ']':
+                    # 遇到右括号，结束当前层次的递归
                     return i, res
                 else:
+                    # 处理字母，直接累加到结果字符串
                     res += s[i]
                 i += 1
             return res
         return dfs(s,0)
-
