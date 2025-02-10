60a7fd00bf88bb3ca4c0dec9e21b1b70a82fa21e
ZapcoMan <3507557934@qq.com>
2025-02-10 23:10:10 +0800

    完成题目

--- 基础/Leetcode/2025-02-10/栈/最小栈.py
+++ 基础/Leetcode/2025-02-10/栈/最小栈.py
@@ -14,3 +14,19 @@ void pop() 删除堆栈顶部的元素。
 int top() 获取堆栈顶部的元素。
 int getMin() 获取堆栈中的最小元素。
 """
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
