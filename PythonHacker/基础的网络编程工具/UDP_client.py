eb1994e860641365f221309198d3e75f385918cf
ZapcoMan <3507557934@qq.com>
2025-02-19 22:09:42 +0800

    UDP 客户端 完成

--- PythonHacker/基础的网络编程工具/UDP_client.py
+++ PythonHacker/基础的网络编程工具/UDP_client.py
@@ -3,3 +3,12 @@
 # @Author  : codervibe
 # @File    : UDP_client.py
 # @Project : pythonBasics
+import socket
+target_host = '127.0.0.1'
+target_port = 8999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto(b'AAABBBCCC', (target_host, target_port))
+data,address = client.recvfrom(4096)
+print(f"{data.decode()}")
+client.close()
