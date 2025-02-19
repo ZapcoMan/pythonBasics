3c3f6e8e7a842c62f372a8086f58743c4152a69a
ZapcoMan <3507557934@qq.com>
2025-02-19 23:06:01 +0800

    完成 NetCat 类 send 方法

--- PythonHacker/基础的网络编程工具/netcat/netcat.py
+++ PythonHacker/基础的网络编程工具/netcat/netcat.py
@@ -11,6 +11,7 @@ import sys
 import textwrap
 import threading
 
+
 class NetCat:
     def __init__(self, args, buffer=None):
         self.args = args
@@ -24,12 +25,37 @@ class NetCat:
         else:
             self.send()
 
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response, end='')
+                    buffer = input('>')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -55,8 +81,3 @@ if __name__ == '__main__':
         buffer = sys.stdin.read()
     nc = NetCat(args, buffer.encode())
     nc.run()
-
-
-
-
-
