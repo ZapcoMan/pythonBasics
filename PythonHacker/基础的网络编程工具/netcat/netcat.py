b89f7355e812ca52d7969a60919e0eadd9cd7365
ZapcoMan <3507557934@qq.com>
2025-02-19 23:08:55 +0800

    调整方法顺序

--- PythonHacker/基础的网络编程工具/netcat/netcat.py
+++ PythonHacker/基础的网络编程工具/netcat/netcat.py
@@ -49,16 +49,6 @@ class NetCat:
             self.socket.close()
             sys.exit()
 
-    def listen(self):
-        self.socket.bind((self.args.target, self.args.port))
-        self.socket.listen(5)
-        while True:
-            client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(
-                target=self.handle, args=(client_socket,)
-            )
-            client_thread.start()
-
     def handle(self, client_socket):
         if self.args.execute:
             output = execute(self.args.execute)
@@ -91,6 +81,18 @@ class NetCat:
                     print(f'Server killed {e}')
                     self.socket.close()
                     sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
+            client_thread.start()
+
+
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
