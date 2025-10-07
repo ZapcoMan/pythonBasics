00257c907a1242a964fca910ed201aa3e9e35f98
codervibe <3217277135@qq.com>
2025-10-07 10:02:06 +0800

    feat(crawler): 添加闲鱼商品详情爬虫的数据文件
    新增单个商品搜索数据的CSV文件，包含价格、标签、产品简介、标题、昵称、区域、标题详情等字段，用于存储爬取的商品信息数据。

--- src/crawler/闲鱼/咸鱼商品详情爬虫/商品搜索爬虫批量.py
+++ src/crawler/闲鱼/咸鱼商品详情爬虫/商品搜索爬虫批量.py
@@ -8,11 +8,17 @@ import hashlib
 
 def get_sign(page_number):
     token = '9a7fd05c49b25d5075fab51a234be307'
-    j = str(int(time.time() * 1000))
+    current_timestamp = int(time.time() * 1000)
+    print(f"当前时间戳: {current_timestamp}")
+    sixteen_hours_ago = current_timestamp - (16 * 60 * 60 * 1000)
+    # j = str(int(time.time() * 1000))
+    j = str(sixteen_hours_ago)
     h = '34839810'
     c_data = '{"pageNumber":%d,"keyword":"python爬虫","fromFilter":false,"rowsPerPage":30,"sortValue":"","sortField":"","customDistance":"","gps":"","propValueStr":{},"customGps":"","searchReqFromPage":"pcSearch","extraFilterValue":"{}","userPositionJson":"{}"}' % page_number
     string = token + "&" + j + "&" + h + "&" + c_data
+    print(string)
     sign = hashlib.md5(string.encode('utf-8')).hexdigest()
+
     return j, c_data, sign
 
 # 打开CSV文件准备写入数据
@@ -33,9 +39,9 @@ csv_writer.writeheader()
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
     "Referer": "https://www.goofish.com/",
-    "Cookie": "t=333d89bdd8d3c7f1680d61c314977359; tracknick=tb575736359; cna=5dFRIU3sMHMBASQJijx6xuFX; isg=BA4O1Z6E6rPx1V7kM_eYllnfX-TQj9KJBFr77zhXA5HMm671oBaHmbdQ18f3hsqh; havana_lgc2_77=eyJoaWQiOjIyMDk5NjgxNDA2MTcsInNnIjoiZjhjNTRhYmNkNWYyM2RiNzIzZTcxYTUxYTUyOWExZGYiLCJzaXRlIjo3NywidG9rZW4iOiIxbkFmbUpERllVQzhoamJuVVBEVjdZdyJ9; _hvn_lgc_=77; havana_lgc_exp=1760719611057; mtop_partitioned_detect=1; _m_h5_tk=d600392e8884098f2d0b81b17c792cdc_1759801420552; _m_h5_tk_enc=748cb5f548dd2a1f2a4a2208fd29dd1d; cookie2=1bd479bb51a7304737dd39912bcce0eb; _samesite_flag_=true; sgcookie=E100lSj5ts%2Bd9eb0HAVpdFmW%2BsVh9FYgXvUZuwcDxh0BtMKbwlSXJGs5OlNCOdjXKrrUB4oRAn7SpNlLypY%2BSmUclm%2Fn0PN6lsfI%2FDaqY7Dmno6l%2F5%2BZX8Jlqq1URukGipG2; csg=2de4d3fd; _tb_token_=3f65e75e137d5; unb=2209968140617; sdkSilent=1759879902293; xlly_s=1; tfstk=gYmjHXw-zsfXMluLllJPP_3Eu2rslL-FCOwtKAIVBoEA61HLajP40dx_6jVr0Sk4D5Is_Aoq_cMGfRHtsKVwnUkmnlq9TBReYxDm8DVhLmwv28HuptQvzrCe5o8JTB-e4EB8fIOE_BjiSzegw-eT6-p52JygX-eTXLN8CJbOMfE9FLw_hNITWGURe-2gX5hTkLM8ZRZTHfE9FYFuBbndNR8bBx9KnBgiwGJY-7sOX0wJsWMfwGySc-wa9xnlHtp8hrNKH7Ox_FcTyA4sjOj_8x3nsJhAMCNiVYnxJXdPuS3QCvusOHIgEqhj2PiyIHMucRaLXz6OXYZ86yizvHC7E4HqWmcfCGem0Dz_tz9OjPr-Yy3KGOvneoetsymwtiVxBvmn8ktft-g-dusPNMPQRW_1Fyj_FWJWFNbGHJo99Z-gHaaYE-KeFL15SreuFWJWFNbgk82x8L95NNf.."
+    "Cookie": "t=333d89bdd8d3c7f1680d61c314977359; tracknick=tb575736359; cna=5dFRIU3sMHMBASQJijx6xuFX; isg=BA4O1Z6E6rPx1V7kM_eYllnfX-TQj9KJBFr77zhXA5HMm671oBaHmbdQ18f3hsqh; cookie2=1bd479bb51a7304737dd39912bcce0eb; _samesite_flag_=true; sgcookie=E100lSj5ts%2Bd9eb0HAVpdFmW%2BsVh9FYgXvUZuwcDxh0BtMKbwlSXJGs5OlNCOdjXKrrUB4oRAn7SpNlLypY%2BSmUclm%2Fn0PN6lsfI%2FDaqY7Dmno6l%2F5%2BZX8Jlqq1URukGipG2; csg=2de4d3fd; _tb_token_=3f65e75e137d5; unb=2209968140617; sdkSilent=1759879902293; xlly_s=1; mtop_partitioned_detect=1; _m_h5_tk=9a7fd05c49b25d5075fab51a234be307_1759808103035; _m_h5_tk_enc=c4118ef3c44e5a8a979739e9dba2c653; tfstk=gBw-nFcf0ZbkVK8y2yfcKGNY-0IcssqzZzr6K20kOrUYfl8oO8DuJBUuYycCzY2LkkaO4DbzK3P4Sl9uE_kHpYkEdNbGIOmz4vkB0iYP2UJjbD6HVLgIUxO_ruBOIOqzV3m5jo6gKL3BIDuIdbGIcKgqvpOQRXgjlqoEAUiBFiEjuqiBRpOSlniixeT7RvsYcqoEVvG7RxsxYqgIdvGkzg32V0pLNHUa4cQlNCRaH0h-Jp0vpVwDIb01eq9ppqzIwQqSkp9QHWdeq235_n0EE4rq2yWwUYGLO8DbFZ6IC5zYpfMOtgkTDSViwSQWpq2ouf2SBH67DY3-1-h6vdz_DkNiM-j1QYHSPWDzxhQuD8Uu4-EHAIM-EShTeA6wP2V0XJnLLwWYWkZaOjepyglzIRHPLF0txQsADBRENmRxupw3Fgv2Bm3GVbOeTXmqDVjADBRENmoxSg9WTBln0"
 }
-for pageNumber in range(1, 2):
+for pageNumber in range(1, 8):
     print(f'正在采集第{pageNumber}页的数据内容')
     url = "https://h5api.m.goofish.com/h5/mtop.taobao.idlemtopsearch.pc.search/1.0/"
     j, c_data, sign = get_sign(pageNumber)
@@ -43,7 +49,7 @@ for pageNumber in range(1, 2):
         "jsv": "2.7.2",
         "appKey": "34839810",
         "t": j,
-        "sign": "8e4f63f339cc4eaea8dc3a9a778a94a5",
+        "sign": sign,
         "v": "1.0",
         "type": "originaljson",
         "accountSite": "xianyu",
@@ -61,8 +67,8 @@ for pageNumber in range(1, 2):
 
     response = requests.post(url, headers=headers, params=params, data=data)
     json_data = response.json()
-    pprint(json_data)
-    exit()
+    # pprint(json_data)
+    # exit()
     resultList = json_data['data']['resultList']
     # pprint(resultList)
     for item in resultList:
